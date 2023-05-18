8a698587535ec86f88de85370804bbd0816d817a
yogurt <eowest@gmail.com>
2023-05-18 18:23:10 -0400

    first config version

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "rust_analyzer",
+        "jedi_language_server",
       })
     end,
   },
@@ -18,8 +20,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "prettier",
+        "stylua",
+        "rust_analyzer",
       })
     end,
   },
@@ -29,7 +32,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "python",
+        "rust",
       })
     end,
   },
